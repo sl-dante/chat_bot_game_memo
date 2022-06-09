ffdc0ad9ea8e2657a15b23b1f637c19be353842c
sl_dante <andanttte@gmail.com>
2022-06-09 22:11:22 +0700

    Добавление обучения по картинкам

--- common_method_for_project.py
+++ common_method_for_project.py
@@ -1,10 +1,21 @@
 import urllib
 from pathlib import Path
+import sqlite3
+from creator_db import Chess
+
+conn = sqlite3.connect("game_memo.db")
+cursor = conn.cursor()
 
 
-def load_photo(photo_path, upload):
+# Загрузка фотографий в вк
+def load_photo(chess, upload):
+    _type = ''
+    if str(chess.type) == '1':
+        _type = 'japan'
+    elif str(chess.type) == '2':
+        _type = 'china'
     absolute_path = Path(__file__).resolve().parents[0]
-    path = f"{absolute_path}\\assets\\img_for_project\\china_chess\\{photo_path}"
+    path = f"{absolute_path}\\assets\\img_for_project\\{_type}_chess\\{chess.path_photo}"
     photo = upload.photo_messages(path)
     owner_id = photo[0]['owner_id']
     photo_id = photo[0]['id']
@@ -12,3 +23,19 @@ def load_photo(photo_path, upload):
     attachment = f'photo{owner_id}_{photo_id}_{access_key}'
 
     return attachment
+
+
+# Запрос к БД для выбора шахмат по имени
+def select_chess_by_name(name):
+    cursor.execute(
+        "SELECT * FROM Chess WHERE Name = ?", (str(name),))
+
+    result = cursor.fetchall()
+    result = result[0]
+    _chess = Chess(_id=result[0],
+                   _name=result[1],
+                   _description=result[2],
+                   _path_photo=result[3],
+                   _type=result[4])
+
+    return _chess
